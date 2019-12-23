cb4772edb5ee24d1c6396eb542f048421904ae66
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-12-23 14:08:16 +0100

    fix: remove scrollPositionRestoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,11 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-    }),
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
