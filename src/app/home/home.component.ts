c5f9713f5ba8833d143e004daf856ecf483f1afc
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-03-18 21:15:57 +0100

    feat: hide twitter timeline on static site
    Do hide the twitter timeline during static site generation with scully

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { isScullyRunning } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'dk-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomeComponent {
+  scullyRunning: boolean = isScullyRunning();
 }
