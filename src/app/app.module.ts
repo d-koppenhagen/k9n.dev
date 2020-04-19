f30f724bbc906f14207c4536550b0dde388954b8
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-04-19 12:35:12 +0200

    feat: timeline animation on scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { A11yModule } from '@angular/cdk/a11y';
 import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 
@@ -49,6 +50,7 @@ import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
     SharedModule,
     TranslocoRootModule,
     ScullyLibModule,
+    AnimateOnScrollModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
