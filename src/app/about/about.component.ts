b1424da69d347ed8254d2757164e7de70875e3de
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-07-24 21:29:50 +0200

    feat: link Interview with agiledrop

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'dk-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
   youtubePlayerWidth: number;
+  @ViewChild('videoBox') videoBox: ElementRef;
 
   constructor() {}
 
@@ -14,16 +22,15 @@ export class AboutComponent implements OnInit {
     const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
+  }
+
+  ngAfterViewInit() {
     this.onResize();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
-    if (window.innerWidth < 800) {
-      this.youtubePlayerWidth = window.innerWidth - 64;
-      console.log(this.youtubePlayerWidth);
-    } else {
-      this.youtubePlayerWidth = undefined;
-    }
+    console.log();
+    this.youtubePlayerWidth = this.videoBox.nativeElement.clientWidth;
   }
 }
