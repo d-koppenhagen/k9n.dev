71c695898df1b5826dc21fab524f321481763cf0
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-05 08:40:27 +0200

    chore(commitlint): allow type `content`

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,17 @@
  * To check all configuration options, please visit
  * https://commitlint.js.org/#/reference-rules
  */
+const angularTypeEnum = require('@commitlint/config-angular-type-enum');
+
+const customTypes = [
+  ...angularTypeEnum.value(),
+  'chore',
+  'content', // specific for adding / changing /removing blog posts comments
+];
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', customTypes],
+  },
 };
