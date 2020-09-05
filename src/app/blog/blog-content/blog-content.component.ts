cafcceeeeac04d8a16bef26d861652367daa7737
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-05 20:10:16 +0200

    feat: reduce amout of share buttons (responsive)

--- src/app/blog/blog-content/blog-content.component.ts
+++ src/app/blog/blog-content/blog-content.component.ts
@@ -2,13 +2,16 @@ import {
   Component,
   OnInit,
   ViewEncapsulation,
+  AfterViewInit,
   AfterViewChecked,
+  ViewChild,
+  ElementRef,
+  HostListener,
 } from '@angular/core';
 import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { NgNavigatorShareService } from 'ng-navigator-share';
 
 import { HighlightService } from '../../shared/highlight.service';
 import { MetaService } from '../../meta.service';
@@ -20,20 +23,22 @@ import { MetaService } from '../../meta.service';
   preserveWhitespaces: true,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class BlogContentComponent implements OnInit, AfterViewChecked {
+export class BlogContentComponent
+  implements OnInit, AfterViewInit, AfterViewChecked {
   post$: Observable<ScullyRoute>;
   location: null;
   shareData: { url: string; description: string } = {
     url: '',
     description: '',
   };
+  shareBtnCnt = 5;
+  @ViewChild('shareBtnBox') shareBtnBox: ElementRef;
 
   constructor(
     private route: ActivatedRoute,
     private srs: ScullyRoutesService,
     private highlightService: HighlightService,
     private metaService: MetaService,
-    private ngNavigatorShareService: NgNavigatorShareService,
   ) {}
 
   /**
@@ -64,7 +69,23 @@ export class BlogContentComponent implements OnInit, AfterViewChecked {
     );
   }
 
+  ngAfterViewInit() {
+    this.onResize();
+  }
+
   editOnGithubLink() {
     return `https://github.com/d-koppenhagen/d-koppenhagen.de/edit/master${location.pathname}.md`;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    console.log(this.shareBtnBox.nativeElement.clientWidth);
+    if (this.shareBtnBox.nativeElement.clientWidth < 410) {
+      this.shareBtnCnt = 3;
+    } else if (this.shareBtnBox.nativeElement.clientWidth < 480) {
+      this.shareBtnCnt = 4;
+    } else {
+      this.shareBtnCnt = 5;
+    }
   }
 }
