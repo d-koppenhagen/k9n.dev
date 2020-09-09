5a73f3489a3c5994f3d06a1bbc3bd8f617962d20
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-09 21:32:38 +0200

    fix: correctly catch unset publishedAt

--- src/app/meta.service.ts
+++ src/app/meta.service.ts
@@ -30,7 +30,7 @@ export class MetaService {
     this.removeAllKnownTags();
     this.setTitle(post.title);
     this.setDescription(post.description);
-    this.createCanonicalURL(post.publishedAt.url);
+    this.createCanonicalURL(post.publishedAt?.url);
     this.createTwitterCardForBlogPost(post);
     this.createOpenGraphProfileForBlogPost(post);
   }
