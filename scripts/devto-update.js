ed507b7cede9e5b80d472f1d18b2372515fd71f8
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-09 19:04:58 +0200

    chore: update dev.to update script

--- scripts/devto-update.js
+++ scripts/devto-update.js
@@ -1,69 +1,136 @@
 const DevToJS = require('dev-to-js');
 const m2j = require('markdown-to-json');
 
+/**
+ * The dev.to API_KEY for the user
+ */
 const API_KEY = 'DEVTO_API_KEY';
+
+/**
+ * This URL is used before deploying on dev.to to point to the absolute paths
+ */
 const BASE_URL = 'https://d-koppenhagen.de';
 
-const DevToClient = new DevToJS({ apiKey: API_KEY });
+/**
+ * This param defines the time between updateing each post
+ */
+const TIME_BETWEEN_UPDATES = 2000;
+
+/**
+ * This function hook is executed before posting the data to dev.to for each article
+ * @param {Object<any>} articleData The JSON representation of the articles meta data + '.content' for the markdown content
+ */
+const preUpdateAll = (articleData) => {
+  const newData = { ...articleData };
+  newData.content = articleData.content
+    .replace(/\]\(\//g, `](${BASE_URL}/`)
+    .replace(/src="\//g, `src="${BASE_URL}/`);
+  return newData;
+};
 
 const articleMap = new Map([
-  [229947, { name: '2020-01-angular-scully', preUpdate: () => {} }],
-  [272387, { name: '2020-03-dig-deeper-into-scully-ssg', preUpdate: () => {} }],
-  [439481, { name: '2020-08-my-development-setup', preUpdate: () => {} }],
+  [
+    229947,
+    {
+      name: '2020-01-angular-scully',
+      /**
+       * preUpdate: (articleData) => {
+       *   return articleData;
+       * }
+       */
+    },
+  ],
+  [272387, { name: '2020-03-dig-deeper-into-scully-ssg' }],
+  [439481, { name: '2020-08-my-development-setup' }],
 ]);
 
+/**
+ * add a delay
+ * @param {number} ms the time to wait
+ */
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
+/**
+ * create the client for the user with the given `apiKey`
+ */
+const DevToClient = new DevToJS({ apiKey: API_KEY });
+
+/**
+ * fetch all articles for the user
+ */
 DevToClient.getMyArticles().then(async (data) => {
+  errors = false;
+  /**
+   * iterate through the articles
+   * for-of-loop is needed instead of forEach, so run the loop synchronously
+   */
   for (const article of data) {
+    // find corresponding articles that should be updated
     const localArticleMatch = articleMap.get(article.id);
     if (!localArticleMatch) {
       return;
     }
 
-    // we need to wait, to be sure not running into 429 Too many requests response
-    await sleep(1000);
+    // wait, to be sure to not run into '429: Too many requests' response
+    await sleep(TIME_BETWEEN_UPDATES);
 
-    console.log('After wait');
-
-    const localArticleMetaRaw = m2j.parse(
+    // parse yaml from markdown file and the content into JSON string
+    const localArticleDataRaw = m2j.parse(
       [`../blog/${localArticleMatch.name}.md`],
       {
         width: 0,
         content: true,
       },
     );
-    const localArticleMeta = JSON.parse(localArticleMetaRaw)[
+
+    // parse JSON string and store the data
+    const localArticleData = JSON.parse(localArticleDataRaw)[
       localArticleMatch.name
     ];
-    const body = localArticleMeta.content
-      .replace(/\]\(\//g, `](${BASE_URL}/`)
-      .replace(/src="\//g, `src="${BASE_URL}/`);
 
+    // execute 'preUpdateAll()' function for each post
+    let revisedLocalArticleData = preUpdateAll(localArticleData);
+
+    // if 'preUpdate' function for modifying a specific post is defined, run it over the data
+    if (localArticleMatch.preUpdate) {
+      revisedLocalArticleData = localArticleMatch.preUpdate(
+        revisedLocalArticleData,
+      );
+    }
+
+    // build the POST object
     const options = {
-      body_markdown: body,
-      title: localArticleMeta.title,
-      published: Boolean(localArticleMeta.published),
-      main_image: `${BASE_URL}/${localArticleMeta.thumbnail.header}`,
-      canonical_url: `${BASE_URL}/blog/${localArticleMatch.name}`,
-      description: localArticleMeta.description,
-      // series: string,
-      // tags: string[],
+      body_markdown: revisedLocalArticleData.content,
+      title: revisedLocalArticleData.title,
+      published: Boolean(revisedLocalArticleData.published),
+      main_image: `${BASE_URL}/${revisedLocalArticleData.thumbnail.header}`,
+      canonical_url: `${BASE_URL}/blog/${revisedLocalArticleData.name}`,
+      description: revisedLocalArticleData.description,
+      // series?: string,
+      // tags?: string[],
       // organization_id?: number
     };
-    // console.log(options.body_markdown);
+
+    // finally update the article
     DevToClient.updateArticle(article.id, options).then(
-      (res) => {
-        // console.log('RESPONSE', res.header);
+      () => {
+        console.log(`✅ Updated article: ${options.title}, id: ${article.id}`);
       },
       (err) => {
-        console.log('ERROR', err);
+        console.error(
+          `❌ Error while trying to update article: ${options.title}, id: ${article.id}`,
+          err,
+        );
+        errors = true;
       },
     );
   }
-  console.log('🎉 finished updateing articles on dev.to');
+
+  errors
+    ? console.log('😓 finished with errors')
+    : console.log('🎉 finished without errors');
 });
