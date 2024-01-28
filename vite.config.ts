a47e5809fbf2f8f1346fa86a2b32a54f8c3fe64e
Danny Koppenhagen <mail@k9n.dev>
2024-01-28 21:18:02 +0100

    chore: update analog

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import analog from '@analogjs/platform';
-import * as fs from 'fs';
+import analog, { PrerenderContentFile } from '@analogjs/platform';
+import { PostAttributes } from 'src/app/types';
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
 
-const contentRoutesRaw = fs.readFileSync('./content-routes.json', 'utf-8');
-const contentRoutes: string[] = JSON.parse(contentRoutesRaw);
+function transFormContentDirRoute(file: PrerenderContentFile, base: string) {
+  console.log('transFormContentDirRoute', base, file)
+  const attributes = file.attributes as PostAttributes
+  console.log('transFormContentDirRoute attributes', attributes)
+  // do not include files marked as draft in frontmatter
+  if (attributes.draft) {
+    return false;
+  }
+  // use the slug from frontmatter if defined, otherwise use the files basename
+  const slug = attributes.slug || file.name;
+  console.log('transFormContentDirRoute slug', slug)
+  return `/${base}/${slug}`;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -19,14 +30,21 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     analog({
       prerender: {
-        routes: async () => [
+        routes: [
           '/api/rss.xml',
           '/',
           '/blog',
           '/contact',
           '/imprint',
           '/projects',
-          ...contentRoutes,
+          {
+            contentDir: './src/content/blog',
+            transform: (file: PrerenderContentFile) => transFormContentDirRoute(file, 'blog'),
+          },
+          {
+            contentDir: './src/content/projects',
+            transform: (file: PrerenderContentFile) => transFormContentDirRoute(file, 'projects'),
+          },
         ],
         postRenderingHooks: [
           async (route) => {
