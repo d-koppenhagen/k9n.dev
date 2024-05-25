c882e8d51817bf87eb9a82c5d894d7ec170158a7
Danny Koppenhagen <mail@k9n.dev>
2024-05-25 22:31:56 +0200

    chore: update gtag script

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import analog, { PrerenderContentFile } from '@analogjs/platform';
 import angular from '@analogjs/vite-plugin-angular';
 import { PostAttributes } from 'src/app/types';
 /// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
 
 function transFormContentDirRoute(file: PrerenderContentFile, base: string) {
@@ -60,14 +59,14 @@ export default defineConfig(({ mode }) => ({
             if (route.route.endsWith('.xml')) {
               return;
             }
-            const gTag = `<script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-              ga('create', 'UA-XXXXXXXXX-1', 'auto');
-              ga('send', 'pageview');
-            </script>`;
+            const gTag = `<!-- Google tag (gtag.js) -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-HY3CPEEH1Z"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-XXXXXXXXX-1');
+              </script>`;
             route.contents = route.contents?.concat(gTag);
           },
         ],
