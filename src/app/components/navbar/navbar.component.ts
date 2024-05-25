8a989370936e6f3227df5c0c71abe658671ce9bc
Danny Koppenhagen <mail@k9n.dev>
2024-05-25 21:00:51 +0200

    fix(a11y): improve menu handling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
-import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { Dialog, DialogModule, DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'dk-navbar',
@@ -10,15 +10,32 @@ import { Dialog, DialogModule } from '@angular/cdk/dialog';
   standalone: true,
 })
 export class NavbarComponent {
-  constructor(public dialog: Dialog) {}
+  private readonly dialog = inject(Dialog);
+  dialogRef = signal<DialogRef<string> | null>(null);
+
+  onKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.openDialog();
+    }
+  }
 
   openDialog(): void {
-    this.dialog.open<string>(MenuComponent, {
-      ariaModal: true,
-      hasBackdrop: true,
-      closeOnNavigation: true,
-      closeOnDestroy: true,
-      disableClose: false,
-    });
+    if (this.dialogRef()) {
+      this.dialog.closeAll();
+      this.dialogRef.set(null);
+    } else {
+      const ref = this.dialog.open<string>(MenuComponent, {
+        ariaModal: true,
+        hasBackdrop: true,
+        closeOnNavigation: true,
+        closeOnDestroy: true,
+        disableClose: false,
+      });
+      this.dialogRef.set(ref);
+      ref.closed.subscribe(() => {
+        this.dialogRef.set(null);
+      });
+    }
   }
 }
