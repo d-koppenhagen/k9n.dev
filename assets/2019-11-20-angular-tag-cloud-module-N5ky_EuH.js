decbb23cff681aa3df6fa5d4213b669b75127164
d-koppenhagen <d-koppenhagen@users.noreply.github.com>
2026-01-20 13:24:35 +0000

    deployed via Analog Publish Github Pages 🚀 for 0fd9f436900ea2362ddcb3e9b75f05ff3017bec3

---
+++ assets/2019-11-20-angular-tag-cloud-module-N5ky_EuH.js
@@ -0,0 +1,22 @@
+const a=`---
+title: angular-tag-cloud-module — Generated word clouds for your Angular app
+description: My module angular-tag-cloud-module lets you generate word clouds / tag clouds for your Angular app
+published: true
+author:
+  name: Danny Koppenhagen
+  mail: mail@k9n.dev
+updated: 2020-04-09
+keywords:
+  - Angular
+  - Angular CLI
+  - word cloud
+  - tag cloud
+language: en
+thumbnail:
+  header: images/projects/angular-tag-cloud-module.png
+  card: images/projects/angular-tag-cloud-module-small.png
+---
+
+<p>My project <strong>angular-tag-cloud-module</strong> gives you an Angular module for setting up a tag cloud / word cloud easily.
+It will take an array of words or links and put them randomly into a container.
+You can specify some styling options for the whole word cloud or for each tag cloud item.</p><p>You can try out the module, and it&#39;s configuration at the <a href="https://d-koppenhagen.github.io/angular-tag-cloud-module">Github Pages Demo Site</a>.</p><p>The official documentation for <em>angular-tag-cloud-module</em> can be found on <a href="https://github.com/d-koppenhagen/angular-tag-cloud-module">Github</a> or <a href="https://www.npmjs.com/package/angular-tag-cloud-module">NPM</a>.</p>`;export{a as default};
---
+++ assets/2023-04-05-vue3-openlayers-Coqjs_RE.js
@@ -0,0 +1,29 @@
+const e=`---
+title: 'Maintainer: vue3-openlayers'
+description: Since April 2023, I am actively maintaining and evolving the vue3-openlayers library — An OpenLayers Wrapper for Vue3.
+published: true
+author:
+  name: Danny Koppenhagen
+  mail: mail@k9n.dev
+updated: 2023-04-05
+keywords:
+  - Vue
+  - Vue3
+  - OpenLayers
+language: en
+thumbnail:
+  header: images/projects/vue3-openlayers.png
+  card: images/projects/vue3-openlayers-small.png
+---
+
+<p>Since April 2023, I am actively maintaining and evolving the <a href="https://vue3openlayers.netlify.app/">vue3-openlayers library</a> — An OpenLayers Wrapper for Vue3.
+My original intention was just to fix some bugs and add some features I needed this time in my current project.
+But after a while using this project I really loved it and was motivated enough to get more involved into the project.
+I contributed actively and got in touch with Melih Altıntaş, the creator of this library.
+He was really supportive and made me an official Maintainer of the project.
+Since then I shifted the whole project to be based on TypeScript, streamlined the APIs and added several new features and bug fixes.</p><ul>
+<li><strong><a href="https://vue3openlayers.netlify.app/">vue3-openlayers Website</a></strong></li>
+<li><strong><a href="https://www.npmjs.com/package/vue3-openlayers">vue3-openlayers NPM Package</a></strong></li>
+<li><strong><a href="https://github.com/MelihAltintas/vue3-openlayers">vue3-openlayers GitHub Repository</a></strong></li>
+</ul>
+`;export{e as default};
