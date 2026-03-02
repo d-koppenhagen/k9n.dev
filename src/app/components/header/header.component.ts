01288e714536a36e060e4bf0732b83fac3c1c154
Danny Koppenhagen <mail@k9n.dev>
2026-03-02 22:08:55 +0100

    fix(a11y): remove interactive text slot animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TextSlotMachineComponent } from '../text-slot-machine/text-slot-machine.component';
 
 @Component({
   selector: 'dk-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  imports: [RouterLink, TextSlotMachineComponent],
+  imports: [RouterLink],
 })
-export class HeaderComponent {
-  expanded = false;
-
-  onMouseEnter() {
-    this.checkScreenWidth();
-  }
-
-  onMouseLeave() {
-    this.expanded = false;
-  }
-
-  private checkScreenWidth() {
-    if (window?.innerHeight) {
-      const screenWidth = window.innerWidth;
-      this.expanded = screenWidth > 900;
-    }
-  }
-}
+export class HeaderComponent {}
